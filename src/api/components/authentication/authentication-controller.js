62d8619278a3c34001015917a2c1a37ce9d24275
fortunecookiees <162522300+fortunecookiees@users.noreply.github.com>
2024-05-05 15:12:59 +0700

    NOMOR 2
    belum tambah comment

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,6 +1,11 @@
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
 
+//mekanisme untuk membatasi failed attempt
+const failedAttempts = new Map();
+const maxAttempts = 5;
+const resetAttempts = 30 * 60 * 1000;
+
 /**
  * Handle login request
  * @param {object} request - Express request object
@@ -12,19 +17,36 @@ async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
-    // Check login credentials
+    let userAttempts = failedAttempts.get(email) || {
+      attempts: 0,
+      lastAttemptTime: 0,
+    };
+    if (
+      userAttempts.attempts >= maxAttempts &&
+      Date.now() - userAttempts.lastAttemptTime < resetAttempts
+    ) {
+      throw errorResponder(
+        errorTypes.FORBIDDEN,
+        'Too many failed login attempts.'
+      );
+    }
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
       password
     );
-
     if (!loginSuccess) {
+      userAttempts.attempts++;
+      userAttempts.lastAttemptTime = Date.now();
+      failedAttempts.set(email, userAttempts);
+
       throw errorResponder(
         errorTypes.INVALID_CREDENTIALS,
         'Wrong email or password'
       );
     }
 
+    failedAttempts.delete(email);
+    
     return response.status(200).json(loginSuccess);
   } catch (error) {
     return next(error);
